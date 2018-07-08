af51aa04178321963bdc5b17286c4e4b6d0bb262
Chris Ford <chrisford51@gmail.com>
2018-07-07 23:21:32 -0700

    cleaned up ready for lesson 9

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var app = new Vue({
+new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    title: 'Becoming a Vue Ninja'
+  },
+  methods: {
+
   }
 })
+
+
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     title: 'Becoming a Vue Ninja',
+//     name: 'Chris',
+//     url: 'http://youtube.com',
+//     classes: ['one', 'two'],
+//   },
+//   methods: {
+//     greet(time) {
+//       return `Hello and good ${time}, ${this.name}`
+//     }
+//   }
+// })
